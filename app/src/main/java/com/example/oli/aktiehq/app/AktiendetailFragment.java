92e85172e2f6481301c3416322b0146d2ca6fa9a
Qliver <olivergol@gmx.de>
2016-02-04 20:07:48 +0100

    Initial commit

---
+++ app/src/main/java/com/example/oli/aktiehq/app/AktiendetailFragment.java
@@ -0,0 +1,33 @@
+package com.example.oli.aktiehq.app;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class AktiendetailFragment extends Fragment {
+
+    public AktiendetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_aktiendetail, container, false);
+
+        // Die AktiendetailActivity wurde über einen Intent aufgerufen
+        // Wir lesen aus dem empfangenen Intent die übermittelten Daten aus
+        Intent empfangenerIntent = getActivity().getIntent();
+        if (empfangenerIntent != null && empfangenerIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            String aktienInfo = empfangenerIntent.getStringExtra(Intent.EXTRA_TEXT);
+            ((TextView) rootView.findViewById(R.id.aktiendetail_text))
+                    .setText(aktienInfo);
+        }
+
+        return rootView;
+    }
+}
